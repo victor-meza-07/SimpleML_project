8d0d1661d6f9d09f7d5bcf5232703cc6b5875c5f
victor-meza-07 <victord.meza@gmail.com>
2020-08-26 07:43:22 -0500

    added initial properties

---
+++ SimpleML/NeuralNetwork/NeuralNetowrkController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SimpleML.NeuralNetwork
+{
+    class NeuralNetowrkController
+    {
+        private int[] layers;
+        private float[][] neurons;
+        private float[][] biases;
+        private float[][][] weights;
+        private int[] activations;
+
+        public float fitness = 0; 
+
+        public NeuralNetowrkController(int[] layers)
+        {
+            this.layers = new int[layers.Length];
+
+            for (int i = 0; i < layers.Length; i++)
+            {
+                this.layers[i] = layers[i];
+            }
+
+            //Add Initialization of neurons, Biases, Weights.
+        }
+    }
+}
--- SimpleML/Program.cs
+++ SimpleML/Program.cs
@@ -6,7 +6,7 @@ namespace SimpleML
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to your simple ML Model");
         }
     }
 }
